dbd7382819fb33d7e5bb9d604a598c7c034a6dfd
Bruno Alves <brunobaiano@users.noreply.github.com>
2022-10-10 13:18:06 -0300

    refactor dataTypePerColumnWithTableInfo to use visit pattern (#27)

--- src/test/java/com/jfeatures/msg/codegen/GenerateDTOTest.java
+++ src/test/java/com/jfeatures/msg/codegen/GenerateDTOTest.java
@@ -1,12 +1,15 @@
 package com.jfeatures.msg.codegen;
 
-import com.squareup.javapoet.JavaFile;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.squareup.javapoet.JavaFile;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.select.Select;
+import org.junit.jupiter.api.Test;
 
 class GenerateDTOTest {
 
@@ -42,5 +45,44 @@ class GenerateDTOTest {
         assertEquals(businessPurposeOfSQL+"DTO", dtoForMultiTableSQL.typeSpec.name);
 
     }
+
+    @Test
+    public void testGenerateDTOWithJoin() throws JSQLParserException, IOException {
+        String sql = "Select name, brand as companyName from emp e inner join company c on e.company_id = c.id";
+        Map<String, String> ddlPerTableName = new HashMap<>();
+        ddlPerTableName.put("emp", "CREATE TABLE emp (id INT, name NVARCHAR(50), company_id INT)");
+        ddlPerTableName.put("company", "CREATE TABLE company (id INT, brand NVARCHAR(50))");
+
+        String businessPurposeOfSQL = "BusinessData";
+
+        JavaFile dtoForMultiTableSQL = GenerateDTO.dtoFromSqlAndDdl(sql, ddlPerTableName, businessPurposeOfSQL);
+
+        assertEquals(businessPurposeOfSQL+"DTO", dtoForMultiTableSQL.typeSpec.name);
+
+    }
+
+    @Test
+    public void testGenerateDTOWith300fields() throws JSQLParserException, IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Select name ");
+        for(int i = 0; i < 300; i++){
+            sb.append(", name").append(" AS name").append(i).append(" ");
+        }
+//        String sql = "Select name, brand as companyName from emp e inner join company c on e.company_id = c.id";
+        sb.append("from emp");
+
+        Map<String, String> ddlPerTableName = new HashMap<>();
+        ddlPerTableName.put("emp", "CREATE TABLE emp (id INT, name NVARCHAR(50), company_id INT)");
+        ddlPerTableName.put("company", "CREATE TABLE company (id INT, brand NVARCHAR(50))");
+
+        String businessPurposeOfSQL = "BusinessData";
+
+        JavaFile dtoForMultiTableSQL = GenerateDTO.dtoFromSqlAndDdl(sb.toString(), ddlPerTableName, businessPurposeOfSQL);
+
+        assertEquals(businessPurposeOfSQL+"DTO", dtoForMultiTableSQL.typeSpec.name);
+        assertEquals(301, dtoForMultiTableSQL.typeSpec.fieldSpecs.size());
+
+    }
+
 
 }
